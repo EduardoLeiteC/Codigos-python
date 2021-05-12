5a567f13b33f0195ef8bcee809ba912d7000fb15
EduardoLeiteC <66190042+EduardoLeiteC@users.noreply.github.com>
2021-05-12 08:23:53 -0400

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def soma (a,b):
+    return a+b
+
+
+# In[2]:
+
+
+def sub(c,d):
+    return c-d
+
+
+# In[3]:
+
+
+def divisao(a,b):
+    return a/b
+
+
+# In[4]:
+
+
+def multi (a,b):
+    return a*b
+
+
+# In[ ]:
+
+
+
+
---
+++ leitura.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+def leitura1(a): 
+    arquivo = open('texto1.txt')
+    b= arquivo.read()
+    return b.count(a)
+
+
+# In[19]:
+
+
+def leitura2(a): 
+    arquivo = open('texto2.txt')
+    b= arquivo.read()
+    return b.count(a)
+
+
+# In[18]:
+
+
+def leitura3(a): 
+    arquivo = open('texto3.txt')
+    b= arquivo.read()
+    return b.count(a)
+
+
+# In[17]:
+
+
+def leitura4(a): 
+    arquivo = open('texto4.txt')
+    b= arquivo.read()
+    return b.count(a)
+
+
+# In[ ]:
+
+
+
+
